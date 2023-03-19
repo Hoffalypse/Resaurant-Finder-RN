70a7677316a77b761f924b4254218b9bbc35ffce
Bryan W <bhoff1980@gmail.com>
2023-03-18 23:35:58 -0600

    took non relevant code out of main file and turned into reusable hook

---
+++ src/components/ResultsList.js
@@ -0,0 +1,15 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const ResultsList = () => {
+  return (
+    <View>
+      <Text>Results List</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    
+})
+export default ResultsList
